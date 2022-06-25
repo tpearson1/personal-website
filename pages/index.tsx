a525e5b04fd8063c7491fb1731302f3b6d0b956f
Thomas Pearson <tompearson2002@gmail.com>
2022-06-25 16:46:37 +1200

    Add favicon (https://favicon.io/favicon-generator/)

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,24 @@ const Home: NextPage = () => {
       <Head>
         <title>Thomas Pearson | Software Developer</title>
         <meta name="description" content="About me" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className="min-h-screen grid place-items-center">
